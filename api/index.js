4b52293a75e686a5eb948a8109e12583d00af34b
Huy Phan <phan.q.huy21@gmail.com>
2024-07-09 11:05:45 -0400

    Added a new route to send customer's a copy of their order receipt
    Purpose was to give customers a digital copy of their receipt in case they lose their physical copy

--- api/index.js
+++ api/index.js
@@ -1,24 +1,28 @@
 import express from "express";
 import twilio from "twilio";
 import bodyParser from "body-parser";
+import cors from "cors";
 import "dotenv/config";
 
 // Route imports
 import sendSMS from "../routes/sendSMS.js";
-import getLogs from "../routes/getLogs.js"
+import getLogs from "../routes/getLogs.js";
+import sendReceiptCopy from "../routes/sendReceiptCopy.js";
 
 // Firebase imports here
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 app.use("/sendSMS", sendSMS);
 app.use("/getLogs", getLogs)
+app.use("/sendReceiptCopy", sendReceiptCopy);
 
 app.get("/", (req, res) => {
     res.send("Hey this is my API running 🥳");
 });
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
---
+++ routes/sendReceiptCopy.js
@@ -0,0 +1,42 @@
+// route that handles SMS sending logic
+import express from "express";
+import twilio from "twilio";
+// import admin from "firebase-admin";
+import app from "../firebaseConfig.js";
+// import serviceAccount from "../serviceAccountKey.json" assert { type: "json" };
+import { getFirestore, doc, collection, setDoc } from "firebase/firestore";
+import "dotenv/config";
+
+const router = express.Router();
+const db = getFirestore(app);
+
+router.post("/", async (request, response) => {
+    try {
+        const accountSid = request.body.accountSid;
+        const authToken = request.body.authToken;
+        const client = twilio(accountSid, authToken);
+
+        const orderForm = request.body.orderForm;
+        const phoneNumber = orderForm.phoneNumber;
+        const customerName = orderForm.customerName;
+        const formattedPhoneNumber = `+1${phoneNumber}`;
+
+        const messageBody = `Winn Cleaners: Hello ${customerName}! \n\n Your order receipt is attached. Thank you for choosing Winn! \n\n ${JSON.stringify(orderForm, null, 4)}`;
+
+        client.messages.create({
+            to: formattedPhoneNumber,
+            from: process.env.TWILIO_PHONE_NUMBER,
+            body: messageBody,
+        });
+
+        response.status(200).send({
+            message: `Message sent successfully`,
+            success: true,
+        });
+    } catch (error) {
+        console.log(error);
+        response.status(500).send(error);
+    }
+});
+
+export default router;
